f112035669b7cf2f28fecdee62e2aa397797e591
Muhammad997 <65116552+Muhammad997@users.noreply.github.com>
2026-07-11 21:46:07 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
 
-const sqlite3=require('sqlite3').verbose();
-const db=new sqlite3.Database('./data/memory.db');
-db.run('CREATE TABLE IF NOT EXISTS memory(user TEXT,message TEXT)');
-module.exports=db;
+const db = new sqlite3.Database('./data/memory.db');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS memory (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user TEXT,
+      message TEXT,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+});
+
+module.exports = db;
