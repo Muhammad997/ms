fc1673a81fbf92e6b2cdbda185a5ed10318057aa
Muhammad997 <65116552+Muhammad997@users.noreply.github.com>
2026-07-11 22:07:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 require('dotenv').config();
 
-const db = require('./database');
 const { askAI } = require('./ai');
 
 async function start() {
   console.log('🚀 KocakAi v9 starting...');
 
-  db.all(
-    "SELECT name FROM sqlite_master WHERE type='table'",
-    [],
-    (err, rows) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Tables:', rows);
-      }
-    }
-  );
-
   try {
     const reply = await askAI(
-      'Perkenalkan dirimu sebagai KocakAi'
+      'Perkenalkan dirimu sebagai KocakAi yang lucu'
     );
 
     console.log('AI Reply:');
